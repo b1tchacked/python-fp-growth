e0d49ec706df6ee4fd79f1ffa9ef44d7421a8e80
Pydi Prasanna Sai Kamanuru <itispydi@gmail.com>
2014-03-10 00:27:11 +0530

    Confidence Calculation Bug Fixed

--- confidenceUtil.py
+++ confidenceUtil.py
@@ -4,20 +4,21 @@ def findConfidentItemSets(supportedItemSets , master, items ):
 
     #Function that sees whether the given support values concur with independent sets
     def isConfidentItemSet ( itemset , support , support1 , tree ):
-        confidenceMeasure = 4
+        confidenceMeasure = 1
         combSupport = 0
         itemset.sort(key=lambda v: items[v], reverse=False)
         i = 1
         for treeNode in tree.nodes(itemset[0]):
             curSupport = treeNode.count
-            cur = treeNode.parent
+            cur = treeNode
             while cur is not None and i < len(itemset):
                 cur = cur.parent
                 if  cur != None and cur.item == itemset[i]:
                     i += 1
             if i >= len(itemset):
                 combSupport += curSupport
-
+            i = 1
+        #print str(itemset) + ":" + str(combSupport)
         if ( abs(combSupport - (support*support1/10)) >= confidenceMeasure ):
             print str(itemset) +  str(combSupport)
             return True
